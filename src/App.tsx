3e4a6b2d4b435c281a2ab6b4f4b910d2c826986a
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2024-05-26 15:14:53 +0200

    App.tsx updated. tailwind-config updated.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import { Button } from './components/ui/button'
-import { NavBar } from './components/nav-bar'
+import { useState, useEffect } from 'react';
+import './index.css';
+import Login from './pages/Login/Login';
+import LoadingScreen from './pages/Loading';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // 2 saniye sonra loading'i false yap
+
+    return () => clearTimeout(timer); // Unmount edildiğinde timer'ı temizle
+  }, []);
 
   return (
     <>
-    <NavBar />
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-      </div>
-    </> 
-  )
+      {loading ? <LoadingScreen/> : <Login />}
+    </>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -70,6 +70,7 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        'fade-in': 'fadein 1.5s ease-out'
       },
     },
   },
